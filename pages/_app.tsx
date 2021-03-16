e154956e52a3b77a24e494e2fcd8dbd9acfb3e33
oshanQQ <onoyama1999910@gmail.com>
2021-03-17 01:01:56 +0900

    Set dummy header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,14 @@ import '../styles/globals.css';
 import { ThemeProvider } from '@material-ui/styles';
 import theme from '../src/theme';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { AppBar, Toolbar } from '@material-ui/core';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <AppBar position="static">
+        <Toolbar></Toolbar>
+      </AppBar>
       <ThemeProvider theme={theme}>
         <CssBaseline>
           <Component {...pageProps} />
