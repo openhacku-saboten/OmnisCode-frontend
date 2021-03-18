8ca8449574fdb022d7cc4f6e1f9be5bd9a7673fc
unknown <ohmoriyusuke@gmail.com>
2021-03-18 18:34:21 +0900

    Fix header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,43 @@
 import { FC } from 'react';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
-import { ThemeProvider } from '@material-ui/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
 import theme from '../src/theme';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { AppBar, Toolbar } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  const useStyles = makeStyles({
+    root: {
+      position: 'fixed',
+      bottom: 0,
+      width: '100%',
+    },
+  });
+  const classes = useStyles();
   return (
     <>
       <AppBar position="static">
-        <Toolbar></Toolbar>
+        <Toolbar>
+          <Typography variant="h4">OmnisCode</Typography>
+          <Grid
+            container
+            alignItems="flex-start"
+            justify="flex-end"
+            direction="row">
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit">
+              <p style={{ fontSize: 'small' }}>UserName</p>
+              <AccountCircle fontSize="large" />
+            </IconButton>
+          </Grid>
+        </Toolbar>
       </AppBar>
       <ThemeProvider theme={theme}>
         <CssBaseline>
