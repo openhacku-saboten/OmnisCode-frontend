2ea25b72d140661adf55975f186bb5b26c043c8f
morioprog <morio.prog@gmail.com>
2021-03-25 02:46:50 +0900

    responsive

--- pages/post/[postid].tsx
+++ pages/post/[postid].tsx
@@ -10,6 +10,7 @@ import {
   FormControlLabel,
   Grid,
   TextField,
+  useMediaQuery,
 } from '@material-ui/core';
 import {
   Timeline,
@@ -21,7 +22,12 @@ import {
 } from '@material-ui/lab';
 import { NextPage, GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import {
+  makeStyles,
+  createStyles,
+  Theme,
+  useTheme,
+} from '@material-ui/core/styles';
 import CommentCard from '../../components/CommentCard';
 import { RequireOne, Comment, CommentType } from '../../src/type';
 import React, { useState } from 'react';
@@ -127,6 +133,9 @@ export const getServerSideProps = ({
 
 const PostDetail: NextPage = (props) => {
   const styles = useStyles();
+
+  const theme = useTheme();
+  const isXsSm = useMediaQuery(theme.breakpoints.down('sm'));
 
   // TODO: resolve error
   const post = props.post as Comment;
@@ -329,6 +338,143 @@ const PostDetail: NextPage = (props) => {
     );
   };
 
+  const formCard: React.FC<Record<string, never>> = () => (
+    <Card className={styles.formCard}>
+      <Grid container direction="column">
+        <Grid item xs={12}>
+          <Editor
+            height="240px"
+            theme="vs-dark"
+            defaultLanguage="markdown"
+            defaultValue={comment}
+            options={{ fontSize: '16px' }}
+            onChange={handleMarkdownEditorChange}
+          />
+        </Grid>
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item>
+              <FormControlLabel
+                className={styles.checkbox}
+                control={
+                  <Checkbox
+                    checked={isHighlight}
+                    onChange={(e) => {
+                      setIsHighlight(e.target.checked);
+                      if (e.target.checked) setIsCommit(false);
+                    }}
+                  />
+                }
+                label="特定の行をハイライトする"
+              />
+            </Grid>
+            {isHighlight && (
+              <Grid item xs={12}>
+                <Grid container alignItems="center" justify="center">
+                  <Grid item xs={4}>
+                    <TextField
+                      defaultValue={highlightFrom}
+                      label="始点"
+                      type="number"
+                      onChange={(e) =>
+                        setHighlightFrom(Math.floor(Number(e.target.value)))
+                      }
+                      error={
+                        highlightFrom < 1 ||
+                        highlightFrom > newest_code_row ||
+                        highlightFrom > highlightTo
+                      }
+                      helperText={
+                        highlightFrom < 1 ||
+                        highlightFrom > newest_code_row ||
+                        highlightFrom > highlightTo
+                          ? `1以上${newest_code_row}以下かつ終点以下の値である必要があります`
+                          : ''
+                      }
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={1}>
+                    <Box style={{ textAlign: 'center' }}>〜</Box>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <TextField
+                      defaultValue={highlightTo}
+                      label="終点"
+                      type="number"
+                      onChange={(e) =>
+                        setHighlightTo(Math.floor(Number(e.target.value)))
+                      }
+                      error={
+                        highlightTo < 1 ||
+                        highlightTo > newest_code_row ||
+                        highlightFrom > highlightTo
+                      }
+                      helperText={
+                        highlightTo < 1 ||
+                        highlightTo > newest_code_row ||
+                        highlightFrom > highlightTo
+                          ? `1以上${newest_code_row}以下かつ始点以上の値である必要があります`
+                          : ''
+                      }
+                      fullWidth
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+            )}
+          </Grid>
+        </Grid>
+        {isMyPost && (
+          <Grid item>
+            <FormControlLabel
+              className={styles.checkbox}
+              control={
+                <Checkbox
+                  checked={isCommit}
+                  onChange={(e) => {
+                    setIsCommit(e.target.checked);
+                    if (e.target.checked) setIsHighlight(false);
+                  }}
+                />
+              }
+              label="ソースコードを更新する"
+            />
+            {isCommit && (
+              <Editor
+                height="360px"
+                theme="vs-dark"
+                defaultLanguage={post.language}
+                defaultValue={committingCode}
+                options={{ fontSize: '16px' }}
+                onChange={handleCommitEditorChange}
+              />
+            )}
+          </Grid>
+        )}
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setModalOpen(true)}
+          className={styles.squareButton}
+          fullWidth>
+          投稿確認 (プレビュー表示)
+        </Button>
+        <Dialog
+          open={modalOpen}
+          onClose={() => setModalOpen(false)}
+          className={styles.dialog}
+          fullWidth
+          maxWidth="md"
+          PaperProps={{
+            style: { borderRadius: '0.5rem' },
+          }}>
+          {previewModal({})}
+        </Dialog>
+      </Grid>
+    </Card>
+  );
+
   return (
     <>
       <Box m={4}>
@@ -340,44 +486,27 @@ const PostDetail: NextPage = (props) => {
           <h1 style={{ textAlign: 'center', marginBottom: '60px' }}>
             {post.title}
           </h1>
-          <Timeline align="left">
-            <TimelineItem>
-              <TimelineOppositeContent className={styles.oppositeContent} />
-              <TimelineSeparator>
-                <Avatar className={`${styles.icon} ${styles.postUserIcon}`}>
-                  {post.user_id}
-                </Avatar>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent>
-                <CommentCard
-                  user_id={post.user_id}
-                  type="post"
-                  content={post.content}
-                  code={post.code}
-                  language={post.language}
-                  source={post.source}
-                />
-              </TimelineContent>
-            </TimelineItem>
-            {comments.map((comment, idx) => {
-              const old_code = cur_code;
-              if (comment.type === 'commit') {
-                cur_code = comment.code;
-              }
-              return (
-                <TimelineItem key={idx}>
-                  <TimelineOppositeContent className={styles.oppositeContent} />
-                  <TimelineSeparator>
-                    <Avatar
-                      className={`${styles.icon} ${
-                        myUserId === comment.user_id ? styles.postUserIcon : ''
-                      }`}>
-                      {comment.user_id}
-                    </Avatar>
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent>
+          {/* TODO: isXsSmならCommentCardにavatar_urlを追加する */}
+          {isXsSm ? (
+            <>
+              <Grid container>
+                <Grid item xs={12}>
+                  <CommentCard
+                    user_id={post.user_id}
+                    type="post"
+                    content={post.content}
+                    code={post.code}
+                    language={post.language}
+                    source={post.source}
+                    is_post_user={true}
+                  />
+                </Grid>
+                {comments.map((comment, idx) => {
+                  const old_code = cur_code;
+                  if (comment.type === 'commit') {
+                    cur_code = comment.code;
+                  }
+                  return (
                     <CommentCard
                       type={comment.type}
                       user_id={comment.user_id}
@@ -388,163 +517,85 @@ const PostDetail: NextPage = (props) => {
                       first_line={comment.first_line}
                       last_line={comment.last_line}
                       old_code={old_code}
+                      is_post_user={post.user_id === comment.user_id}
+                      key={idx}
                     />
-                  </TimelineContent>
-                </TimelineItem>
-              );
-            })}
-            <TimelineItem>
-              <TimelineOppositeContent className={styles.oppositeContent} />
-              <TimelineSeparator>
-                <Avatar className={styles.icon}>
-                  <AddIcon />
-                </Avatar>
-              </TimelineSeparator>
-              <TimelineContent>
-                <Card className={styles.formCard}>
-                  <Grid container direction="column">
-                    <Grid item xs={12}>
-                      <Editor
-                        height="240px"
-                        theme="vs-dark"
-                        defaultLanguage="markdown"
-                        defaultValue={comment}
-                        options={{ fontSize: '16px' }}
-                        onChange={handleMarkdownEditorChange}
+                  );
+                })}
+                {formCard({})}
+              </Grid>
+            </>
+          ) : (
+            <Timeline align="left">
+              <TimelineItem>
+                <TimelineOppositeContent className={styles.oppositeContent} />
+                <TimelineSeparator>
+                  <Avatar className={`${styles.icon} ${styles.postUserIcon}`}>
+                    {post.user_id}
+                  </Avatar>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>
+                  <CommentCard
+                    user_id={post.user_id}
+                    type="post"
+                    content={post.content}
+                    code={post.code}
+                    language={post.language}
+                    source={post.source}
+                    is_post_user={true}
+                  />
+                </TimelineContent>
+              </TimelineItem>
+              {comments.map((comment, idx) => {
+                const old_code = cur_code;
+                if (comment.type === 'commit') {
+                  cur_code = comment.code;
+                }
+                return (
+                  <TimelineItem key={idx}>
+                    <TimelineOppositeContent
+                      className={styles.oppositeContent}
+                    />
+                    <TimelineSeparator>
+                      <Avatar
+                        className={`${styles.icon} ${
+                          myUserId === comment.user_id
+                            ? styles.postUserIcon
+                            : ''
+                        }`}>
+                        {comment.user_id}
+                      </Avatar>
+                      <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                      <CommentCard
+                        type={comment.type}
+                        user_id={comment.user_id}
+                        content={comment.content}
+                        source={comment.source}
+                        language={post.language}
+                        code={cur_code}
+                        first_line={comment.first_line}
+                        last_line={comment.last_line}
+                        old_code={old_code}
+                        is_post_user={post.user_id === comment.user_id}
                       />
-                    </Grid>
-                    <Grid item>
-                      <Grid container direction="column">
-                        <Grid item>
-                          <FormControlLabel
-                            className={styles.checkbox}
-                            control={
-                              <Checkbox
-                                checked={isHighlight}
-                                onChange={(e) => {
-                                  setIsHighlight(e.target.checked);
-                                  if (e.target.checked) setIsCommit(false);
-                                }}
-                              />
-                            }
-                            label="特定の行をハイライトする"
-                          />
-                        </Grid>
-                        {isHighlight && (
-                          <Grid item xs={12}>
-                            <Grid
-                              container
-                              alignItems="center"
-                              justify="center">
-                              <Grid item xs={4}>
-                                <TextField
-                                  defaultValue={highlightFrom}
-                                  label="始点"
-                                  type="number"
-                                  onChange={(e) =>
-                                    setHighlightFrom(
-                                      Math.floor(Number(e.target.value))
-                                    )
-                                  }
-                                  error={
-                                    highlightFrom < 1 ||
-                                    highlightFrom > newest_code_row ||
-                                    highlightFrom > highlightTo
-                                  }
-                                  helperText={
-                                    highlightFrom < 1 ||
-                                    highlightFrom > newest_code_row ||
-                                    highlightFrom > highlightTo
-                                      ? `1以上${newest_code_row}以下かつ終点以下の値である必要があります`
-                                      : ''
-                                  }
-                                  fullWidth
-                                />
-                              </Grid>
-                              <Grid item xs={1}>
-                                <Box style={{ textAlign: 'center' }}>〜</Box>
-                              </Grid>
-                              <Grid item xs={4}>
-                                <TextField
-                                  defaultValue={highlightTo}
-                                  label="終点"
-                                  type="number"
-                                  onChange={(e) =>
-                                    setHighlightTo(
-                                      Math.floor(Number(e.target.value))
-                                    )
-                                  }
-                                  error={
-                                    highlightTo < 1 ||
-                                    highlightTo > newest_code_row ||
-                                    highlightFrom > highlightTo
-                                  }
-                                  helperText={
-                                    highlightTo < 1 ||
-                                    highlightTo > newest_code_row ||
-                                    highlightFrom > highlightTo
-                                      ? `1以上${newest_code_row}以下かつ始点以上の値である必要があります`
-                                      : ''
-                                  }
-                                  fullWidth
-                                />
-                              </Grid>
-                            </Grid>
-                          </Grid>
-                        )}
-                      </Grid>
-                    </Grid>
-                    {isMyPost && (
-                      <Grid item>
-                        <FormControlLabel
-                          className={styles.checkbox}
-                          control={
-                            <Checkbox
-                              checked={isCommit}
-                              onChange={(e) => {
-                                setIsCommit(e.target.checked);
-                                if (e.target.checked) setIsHighlight(false);
-                              }}
-                            />
-                          }
-                          label="ソースコードを更新する"
-                        />
-                        {isCommit && (
-                          <Editor
-                            height="360px"
-                            theme="vs-dark"
-                            defaultLanguage={post.language}
-                            defaultValue={committingCode}
-                            options={{ fontSize: '16px' }}
-                            onChange={handleCommitEditorChange}
-                          />
-                        )}
-                      </Grid>
-                    )}
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={() => setModalOpen(true)}
-                      className={styles.squareButton}
-                      fullWidth>
-                      投稿確認 (プレビュー表示)
-                    </Button>
-                    <Dialog
-                      open={modalOpen}
-                      onClose={() => setModalOpen(false)}
-                      className={styles.dialog}
-                      fullWidth
-                      maxWidth="md"
-                      PaperProps={{
-                        style: { borderRadius: '0.5rem' },
-                      }}>
-                      {previewModal({})}
-                    </Dialog>
-                  </Grid>
-                </Card>
-              </TimelineContent>
-            </TimelineItem>
-          </Timeline>
+                    </TimelineContent>
+                  </TimelineItem>
+                );
+              })}
+              <TimelineItem>
+                <TimelineOppositeContent className={styles.oppositeContent} />
+                <TimelineSeparator>
+                  <Avatar className={styles.icon}>
+                    <AddIcon />
+                  </Avatar>
+                </TimelineSeparator>
+                <TimelineContent>{formCard({})}</TimelineContent>
+              </TimelineItem>
+            </Timeline>
+          )}
         </Container>
       </Box>
     </>
