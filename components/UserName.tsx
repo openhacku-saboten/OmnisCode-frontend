b21d59b47e9e6cf09eb06822932758d126545243
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-03-30 23:20:31 +0900

    Fix header

--- components/UserName.tsx
+++ components/UserName.tsx
@@ -2,14 +2,21 @@ import { firebaseConfig } from '../utils/firebaseConfig';
 import axios from '../utils/axios';
 import firebase from 'firebase/app';
 import React from 'react';
-import { Link, Typography, useTheme, useMediaQuery } from '@material-ui/core';
+import {
+  Link,
+  Typography,
+  useTheme,
+  useMediaQuery,
+  Button,
+} from '@material-ui/core';
+
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
 export function UserName(): JSX.Element {
   const theme = useTheme();
   const showName = useMediaQuery(theme.breakpoints.up('md'));
-  if (!showName) {
-    return <></>;
-  }
+
+  const userIconImage = localStorage.getItem('userIconImage');
   const userName = localStorage.getItem('userName');
   const userId = localStorage.getItem('userId');
   const refreshToken = localStorage.getItem('refreshToken');
@@ -55,21 +62,77 @@ export function UserName(): JSX.Element {
   }
 
   if (userName) {
+    // 画面幅狭い
+    if (!showName) {
+      return (
+        <>
+          <Button
+            variant="contained"
+            href="/post/new"
+            style={{
+              fontSize: 24,
+              fontWeight: 'bold',
+              backgroundColor: '#e9f6fe',
+              borderRadius: 20,
+              margin: 5,
+            }}>
+            投稿
+          </Button>
+          <Link href={'/user/' + userId}>
+            <img
+              src={userIconImage}
+              alt="画像"
+              style={{ marginRight: '10px', borderRadius: '50%' }}
+            />
+          </Link>
+        </>
+      );
+    }
     return (
-      <Link href={'/user/' + userId}>
-        <Typography
+      <>
+        <Button
+          variant="contained"
+          href="/post/new"
           style={{
-            fontSize: 'medium',
-            textDecoration: 'none !important',
-            color: '#ffffff',
+            fontSize: 24,
+            fontWeight: 'bold',
+            backgroundColor: '#e9f6fe',
+            borderRadius: 20,
+            margin: 7,
           }}>
-          {userName}
-        </Typography>
-      </Link>
+          投稿
+        </Button>
+        <Link href={'/user/' + userId}>
+          <Typography
+            style={{
+              fontSize: 'medium',
+              textDecoration: 'none !important',
+              color: '#ffffff',
+            }}>
+            <img
+              src={userIconImage}
+              alt="画像"
+              style={{ marginRight: '10px', borderRadius: '50%' }}
+            />
+            {userName}
+          </Typography>
+        </Link>
+      </>
     );
   } else {
+    // 画面幅狭い
+    if (!showName) {
+      return (
+        <>
+          <Link href="/login">
+            <AccountCircle fontSize="large" style={{ marginRight: '10px' }} />
+          </Link>
+        </>
+      );
+    }
     return (
       <Link href="/login">
+        <AccountCircle fontSize="large" style={{ marginRight: '10px' }} />
         <Typography style={{ fontSize: 'medium', color: '#ffffff' }}>
           ログイン
         </Typography>
